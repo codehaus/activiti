780d86d32b02a567eb86e06082d855c45b67333d
falko.menge <falko.menge@51b7ce9a-3489-0410-86de-dc9a27cf12c7>
2010-08-19 19:18:44 +0000

    - Added infrastructure for JSON transformations - Added transformation to extract a Pool from a BPMN Collaboration

---
+++ activiti/branches/cycle/modules/activiti-cycle/src/main/java/org/activiti/cycle/transform/OryxTransformation.java
@@ -0,0 +1,19 @@
+package org.activiti.cycle.transform;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.oryxeditor.server.diagram.Diagram;
+import org.oryxeditor.server.diagram.DiagramBuilder;
+import org.oryxeditor.server.diagram.JSONBuilder;
+
+public abstract class OryxTransformation implements JsonTransformation {
+
+	public JSONObject transform(JSONObject json) throws JSONException {
+		Diagram diagram = DiagramBuilder.parseJson(json);
+		diagram = transform(diagram);
+		return JSONBuilder.parseModel(diagram);
+	}
+
+	public abstract Diagram transform(Diagram diagram);
+
+}
