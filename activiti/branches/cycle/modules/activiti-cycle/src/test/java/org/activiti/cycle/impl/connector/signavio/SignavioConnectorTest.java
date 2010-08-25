923b251cd972718b4a8b247a352948602e13695a
hawky4s <hawky4s@51b7ce9a-3489-0410-86de-dc9a27cf12c7>
2010-08-25 15:41:41 +0000

    Reverted SignavioConnectorTest which I accidentially submitted

--- activiti/branches/cycle/modules/activiti-cycle/src/test/java/org/activiti/cycle/impl/connector/signavio/SignavioConnectorTest.java
+++ activiti/branches/cycle/modules/activiti-cycle/src/test/java/org/activiti/cycle/impl/connector/signavio/SignavioConnectorTest.java
@@ -1,101 +1,38 @@
 package org.activiti.cycle.impl.connector.signavio;
 
-import java.io.BufferedInputStream;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.util.Calendar;
 
-import org.junit.Ignore;
+import org.json.JSONException;
 import org.junit.Test;
-import org.restlet.data.Response;
 
-// TODO: We need a way to start 
 public class SignavioConnectorTest {
 
   @Test
   public void test() {
     // TODO: Write :-)
   }
-
-  @Ignore
+  
   @Test
-  public void testCreateModelIncludingSvg() throws IOException {
-    String file = "c:/bpmn20json.json";
-    // String file = "c:/createmodel.json.txt";
-    // String file =
-    // "c:/Name myTestModel Time_ Tue Aug 10 08_43_43 CEST 2010 _ Signavio.json.txt";
-
-    String jsonData = readFileAsString(file);
-
-    SignavioConnectorConfiguration sigConf = new SignavioConnectorConfiguration();
-    SignavioConnector sigCon = new SignavioConnector(sigConf);
-
-    sigCon.login("christian.lipphardt@camunda.com", "cam123");
-
-    sigCon.createNewModel("b4ccfc5748dc482db621c7075aedd5df", "myTestModel - " + Calendar.getInstance().getTime(), jsonData, "testSVG", "testSVG");
-  }
-
-  @Ignore
-  @Test
-  public void testDifferentRepresentations() throws IOException {
-    // String signavioModelId = "b68bc46a4879425e8afb4581bffc2f52";
-    // String signavioUrl = "http://127.0.0.1:8080";
-    // SignavioConnectorConfiguration signavioConf = new
-    // SignavioConnectorConfiguration();
-    // SignavioConnector signavioCon = new SignavioConnector(signavioConf);
-    // signavioCon.login("christian.lipphardt@camunda.com", "cam123");
-    // Response signavioResponse =
-    // signavioCon.getJsonResponse(signavioConf.getModelUrl() +
-    // signavioModelId);
-    // System.out.println(signavioResponse.getEntity().getText());
-
-    // String oryxModelId = "";
-    // String oryxUrl = "";
-    // SignavioConnectorConfiguration oryxConf = new
-    // SignavioConnectorConfiguration();
-    // SignavioConnector oryxCon = new SignavioConnector(oryxConf);
-    // Response oryxResponse = oryxCon.getJsonResponse(oryxConf.getModelUrl() +
-    // oryxModelId);
-    // System.out.println(oryxResponse.getEntity().getText());
-
-    // String activitiModelId = "1a4deb43e4434738ab30153e119a4b90";
-    String activitiModelId = "root-directory;testVersionModeler.oryx.xml";
-    String activitiUrl = "http://localhost:8080/activiti-modeler";
-    SignavioConnectorConfiguration activitiConf = new SignavioConnectorConfiguration(activitiUrl);
-    SignavioConnector activitiCon = new SignavioConnector(activitiConf);
-    Response activitiResponse = activitiCon.getJsonResponse(activitiConf.getModelUrl(activitiModelId));
-    System.out.println(activitiResponse.getEntity().getText());
-  }
-
-  @Ignore
-  @Test
-  public void testPdfRepresentation() throws IOException {
-    String activitiModelId = "root-directory;testVersionModeler.oryx.xml";
-    String activitiUrl = "http://localhost:8080/activiti-modeler";
-    SignavioConnectorConfiguration activitiConf = new SignavioConnectorConfiguration(activitiUrl);
-    SignavioConnector activitiCon = new SignavioConnector(activitiConf);
-    Response activitiResponse = activitiCon.getJsonResponse(activitiConf.getModelUrl(activitiModelId) + "/pdf");
-    FileOutputStream fos = new FileOutputStream("c:/pdftest.pdf");
-    fos.write(activitiResponse.getEntity().getText().getBytes());
-    fos.close();
-  }
-
-  private static String readFileAsString(String filePath) throws java.io.IOException {
-    byte[] buffer = new byte[(int) new File(filePath).length()];
-    BufferedInputStream f = null;
-    try {
-      f = new BufferedInputStream(new FileInputStream(filePath));
-      f.read(buffer);
-    } finally {
-      if (f != null) {
-        try {
-          f.close();
-        } catch (IOException ignored) {
-        }
-      }
-    }
-    return new String(buffer, "UTF-8");
+  public void testTransformation() throws IOException, JSONException {
+    // NArf, we need a running signavio for this :-( So skipped for the moment
+    
+    // InputStream is =
+    // this.getClass().getResourceAsStream("/org/activiti/cycle/impl/transform/signavio/engine-pool.json");
+    // BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+    // StringBuilder sb = new StringBuilder();
+    // String line = null;
+    // while ((line = reader.readLine()) != null) {
+    // sb.append(line + "\n");
+    // }
+    // is.close();
+    //
+    // String json = new
+    // SignavioConnector(null).transformJsonToBpmn20Xml(sb.toString());
+    // String transformedJson = new
+    // ExchangeSignavioUuidWithNameTransformation().transform(new
+    // JSONObject(json)).toString();
+    //    
+    // System.out.println(json);
+    // System.out.println(transformedJson);
   }
 }
