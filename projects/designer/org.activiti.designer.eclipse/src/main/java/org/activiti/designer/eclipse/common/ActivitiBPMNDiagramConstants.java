76bacefc3ff9b850b24805cb9426b3f5eba993a4
yvoswillens <yvoswillens@51b7ce9a-3489-0410-86de-dc9a27cf12c7>
2011-01-23 15:07:32 +0000

    OPEN - issue ACT-343: Implement multi page diagram editor http://jira.codehaus.org/browse/ACT-343
    Multi page editor can be openend by either selecting the Activiti Diagram of Activiti BPMN 2.0 XML file.
    Multi page editor is now the default editor for .activiti and .bpmn20.xml files.

--- projects/designer/org.activiti.designer.eclipse/src/main/java/org/activiti/designer/eclipse/common/ActivitiBPMNDiagramConstants.java
+++ projects/designer/org.activiti.designer.eclipse/src/main/java/org/activiti/designer/eclipse/common/ActivitiBPMNDiagramConstants.java
@@ -15,6 +15,7 @@ public final class ActivitiBPMNDiagramConstants {
   // public static final String DIAGRAM_EDITOR_ID =
   // "org.activiti.designer.editor.diagram";
   public static final String DIAGRAM_EDITOR_ID = "org.activiti.designer.editor.multiPageEditor";
+  public static final String BPMN2_CONTENTTYPE_ID = "org.activiti.designer.editor.bpmn.contenttype";
 
   public static final String BPMN_MARSHALLER_NAME = "Activiti Designer BPMN 2.0";
   public static final String BPMN_VALIDATOR_ID = "ActivitiDesignerBPMNValidator";
--- projects/designer/org.activiti.designer.eclipse/src/main/java/org/activiti/designer/eclipse/editor/ActivitiMultiPageEditor.java
+++ projects/designer/org.activiti.designer.eclipse/src/main/java/org/activiti/designer/eclipse/editor/ActivitiMultiPageEditor.java
@@ -1,15 +1,21 @@
 package org.activiti.designer.eclipse.editor;
 
+import org.activiti.designer.eclipse.common.ActivitiBPMNDiagramConstants;
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IMarker;
 import org.eclipse.core.resources.IResourceChangeEvent;
 import org.eclipse.core.resources.IResourceChangeListener;
 import org.eclipse.core.resources.IWorkspace;
 import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.Path;
+import org.eclipse.core.runtime.content.IContentDescription;
+import org.eclipse.core.runtime.content.IContentType;
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.graphiti.mm.pictograms.Diagram;
+import org.eclipse.graphiti.ui.editor.DiagramEditorFactory;
 import org.eclipse.graphiti.ui.editor.DiagramEditorInput;
 import org.eclipse.jface.dialogs.ErrorDialog;
 import org.eclipse.swt.widgets.Display;
@@ -26,174 +32,221 @@ import org.eclipse.wst.sse.ui.StructuredTextEditor;
 
 /**
  * @author Yvo Swillens
- * @since 0.6.0
+ * @since 0.6.1
  * @version 1
  * 
  */
-public class ActivitiMultiPageEditor extends MultiPageEditorPart implements
-		IResourceChangeListener {
+public class ActivitiMultiPageEditor extends MultiPageEditorPart implements IResourceChangeListener {
 
-	private static final String DIAGRAM_PANE_TILE = "Diagram";
-	private static final String XML_PANE_TITLE = "BPMN2.0";
+  private static final String DIAGRAM_PANE_TILE = "Diagram";
+  private static final String XML_PANE_TITLE = "BPMN2.0";
 
-	/** The diagram editor used in page 0. */
-	private ActivitiDiagramEditor diagramEditor;
+  /** The diagram editor used in page 0. */
+  private ActivitiDiagramEditor diagramEditor;
 
-	/** The XML editor used in page 1. */
-	private StructuredTextEditor bpmnEditor;
+  /** The XML editor used in page 1. */
+  private StructuredTextEditor bpmnEditor;
 
-	/**
-	 * Creates a multi-page editor example.
-	 */
-	public ActivitiMultiPageEditor() {
-		super();
-		ResourcesPlugin.getWorkspace().addResourceChangeListener(this);
-	}
+  private IFile associatedBPMN2File;
 
-	private void createDiagramPage() {
+  /**
+   * Creates a multi-page editor example.
+   */
+  public ActivitiMultiPageEditor() {
+    super();
+    ResourcesPlugin.getWorkspace().addResourceChangeListener(this);
+  }
 
-		try {
-			diagramEditor = new ActivitiDiagramEditor();
-			int index = addPage(diagramEditor, getEditorInput());
-			setPageText(index, ActivitiMultiPageEditor.DIAGRAM_PANE_TILE);
-		} catch (PartInitException e) {
-			ErrorDialog.openError(getSite().getShell(),
-					"Error creating nested Activiti Diagram editor", null,
-					e.getStatus());
-		}
-	}
+  private void createDiagramPage() {
 
-	private void createBPMN2Page() {
-		try {
-			bpmnEditor = new StructuredTextEditor();
-			// bpmnEditor.setEditorPart(this);
+    try {
+      diagramEditor = new ActivitiDiagramEditor();
+      int index = addPage(diagramEditor, getEditorInput());
+      setPageText(index, ActivitiMultiPageEditor.DIAGRAM_PANE_TILE);
+    } catch (PartInitException e) {
+      ErrorDialog.openError(getSite().getShell(), "Error creating nested Activiti Diagram editor", null, e.getStatus());
+    }
+  }
 
-			int index = addPage(bpmnEditor, new FileEditorInput(
-					getAssociatedBPMN2IFile()));
+  private void createBPMN2Page() {
+    try {
+      bpmnEditor = new StructuredTextEditor();
+      int index = addPage(bpmnEditor, getBPMN2EditorInput());
+      setPageText(index, ActivitiMultiPageEditor.XML_PANE_TITLE);
+    } catch (PartInitException e) {
+      ErrorDialog.openError(getSite().getShell(), "Error creating nested Activiti BPMN2.0 editor", null, e.getStatus());
+    }
+  }
+  /**
+   * Creates the pages of the multi-page editor.
+   */
+  protected void createPages() {
+    createDiagramPage();
+    createBPMN2Page();
+  }
 
-			setPageText(index, ActivitiMultiPageEditor.XML_PANE_TITLE);
-		} catch (PartInitException e) {
-			ErrorDialog.openError(getSite().getShell(),
-					"Error creating nested Activiti BPMN2.0 editor", null,
-					e.getStatus());
-		}
-	}
+  public void dispose() {
+    ResourcesPlugin.getWorkspace().removeResourceChangeListener(this);
+    super.dispose();
+  }
 
-	/**
-	 * Creates the pages of the multi-page editor.
-	 */
-	protected void createPages() {
-		createDiagramPage();
-		createBPMN2Page();
-	}
-
-	public void dispose() {
-		ResourcesPlugin.getWorkspace().removeResourceChangeListener(this);
-		super.dispose();
-	}
-
-	public void doSave(IProgressMonitor monitor) {
-	  int activePage = getActivePage();
-	  if(activePage == 0) {
-	    getEditor(0).doSave(monitor);
-	  } else if(activePage == 1) {
-	    getEditor(1).doSave(monitor);
-	  }
-	}
-
-	public void doSaveAs() {
-	  int activePage = getActivePage();
+  public void doSave(IProgressMonitor monitor) {
+    int activePage = getActivePage();
     if(activePage == 0) {
-  		IEditorPart editor = getEditor(0);
-  		editor.doSaveAs();
-  		setPageText(0, editor.getTitle());
-  		setInput(editor.getEditorInput());
+      getEditor(0).doSave(monitor);
+    } else if(activePage == 1) {
+      getEditor(1).doSave(monitor);
+    }
+  }
+
+  public void doSaveAs() {
+    int activePage = getActivePage();
+    if(activePage == 0) {
+      IEditorPart editor = getEditor(0);
+      editor.doSaveAs();
+      setPageText(0, editor.getTitle());
+      setInput(editor.getEditorInput());
     } else if(activePage == 1) {
       IEditorPart editor = getEditor(1);
       editor.doSaveAs();
       setPageText(1, editor.getTitle());
       setInput(editor.getEditorInput());
     }
-	}
+  }
 
-	public void gotoMarker(IMarker marker) {
-		setActivePage(0);
-		IDE.gotoMarker(getEditor(0), marker);
-	}
+  public void gotoMarker(IMarker marker) {
+    setActivePage(0);
+    IDE.gotoMarker(getEditor(0), marker);
+  }
 
-	public void init(IEditorSite site, IEditorInput editorInput)
-			throws PartInitException {
-		if (!(editorInput instanceof IFileEditorInput) && !(editorInput instanceof DiagramEditorInput))
-			throw new PartInitException(
-					"Invalid Input: Must be IFileEditorInput");
-		super.init(site, editorInput);
-	}
+  public void init(IEditorSite site, IEditorInput editorInput) throws PartInitException {
+    if (!(editorInput instanceof IFileEditorInput) && !(editorInput instanceof DiagramEditorInput))
+      throw new PartInitException("Invalid Input: Must be Activiti Diagram or BPMN2.0 XML");
 
-	public boolean isSaveAsAllowed() {
-		return true;
-	}
+    // checks if the editor was opened with non Diagram file
+    if (editorInput instanceof IFileEditorInput) {
 
-	public ActivitiDiagramEditor getActivitiDiagramEditor() {
-		IEditorPart editor = getEditor(0);
-		return (ActivitiDiagramEditor) editor;
-	}
+      if (isBPM2FileType((IFileEditorInput) editorInput)) {
+        associatedBPMN2File = ((IFileEditorInput) editorInput).getFile();
+        final IFile diagramFile = getAssociatedDiagramIFile(associatedBPMN2File);
+        final IEditorInput diagramFileEditorInput = new FileEditorInput(diagramFile);
 
-	/**
-	 * Closes all project files on project close.
-	 */
-	public void resourceChanged(final IResourceChangeEvent event) {
-		if (event.getType() == IResourceChangeEvent.PRE_CLOSE) {
-			Display.getDefault().asyncExec(new Runnable() {
-				public void run() {
-					IWorkbenchPage[] pages = getSite().getWorkbenchWindow()
-							.getPages();
-					for (int i = 0; i < pages.length; i++) {
-						if (((FileEditorInput) bpmnEditor.getEditorInput())
-								.getFile().getProject()
-								.equals(event.getResource())) {
-							IEditorPart editorPart = pages[i]
-									.findEditor(bpmnEditor.getEditorInput());
-							pages[i].closeEditor(editorPart, true);
-						}
-					}
-				}
-			});
-		}
-	}
+        // creates DiagramEditorInput from FileEditorInput
+        editorInput = new DiagramEditorFactory().createEditorInput(diagramFileEditorInput);
+      }
+    }
+    super.init(site, editorInput);
+  }
 
-	/**
-	 * Reloads contents of BPMN2 pane when selected
-	 */
-	protected void pageChange(int newPageIndex) {
-		super.pageChange(newPageIndex);
-		if (newPageIndex == 1) {
-			bpmnEditor.setInput(new FileEditorInput(getAssociatedBPMN2IFile()));
-		}
-	}
+  public boolean isSaveAsAllowed() {
+    return true;
+  }
 
-	private IFile getAssociatedBPMN2IFile() {
+  public ActivitiDiagramEditor getActivitiDiagramEditor() {
+    IEditorPart editor = getEditor(0);
+    return (ActivitiDiagramEditor) editor;
+  }
 
-		URI uri = getAssociatedBPMN2URI();
+  /**
+   * Closes all project files on project close.
+   */
+  public void resourceChanged(final IResourceChangeEvent event) {
+    if (event.getType() == IResourceChangeEvent.PRE_CLOSE) {
+      Display.getDefault().asyncExec(new Runnable() {
 
-		final IWorkspace workspace = ResourcesPlugin.getWorkspace();
-		final IFile bpmn20File = workspace.getRoot().getFile(
-				new Path(uri.toPlatformString(true)));
+        public void run() {
+          IWorkbenchPage[] pages = getSite().getWorkbenchWindow().getPages();
+          for (int i = 0; i < pages.length; i++) {
+            if (((FileEditorInput) bpmnEditor.getEditorInput()).getFile().getProject().equals(event.getResource())) {
+              IEditorPart editorPart = pages[i].findEditor(bpmnEditor.getEditorInput());
+              pages[i].closeEditor(editorPart, true);
+            }
+          }
+        }
+      });
+    }
+  }
 
-		return bpmn20File;
-	}
+  /**
+   * Reloads contents of BPMN2 pane when selected
+   */
+  protected void pageChange(int newPageIndex) {
+    super.pageChange(newPageIndex);
+    if (newPageIndex == 1) {
+      bpmnEditor.setInput(getBPMN2EditorInput());
+    }
+  }
 
-	private URI getAssociatedBPMN2URI() {
+  private IFile getAssociatedBPMN2IFile() {
 
-		final Diagram diagram = ((DiagramEditorInput) diagramEditor
-				.getEditorInput()).getDiagram();
-		final URI originalURI = diagram.eResource().getURI();
-		final URI parentURI = originalURI.trimSegments(1);
-		final String REGEX_FILENAME = "\\" + "$originalFile" + "";
+    URI uri = getAssociatedBPMN2URI();
 
-		String finalSegment = "$originalFile" + ".bpmn20.xml";
-		finalSegment = finalSegment.replaceAll(REGEX_FILENAME,
-				originalURI.lastSegment());
+    final IWorkspace workspace = ResourcesPlugin.getWorkspace();
+    final IFile bpmn20File = workspace.getRoot().getFile(new Path(uri.toPlatformString(true)));
 
-		return parentURI.appendSegment(finalSegment);
-	}
+    return bpmn20File;
+  }
+
+  private URI getAssociatedBPMN2URI() {
+
+    final Diagram diagram = ((DiagramEditorInput) diagramEditor.getEditorInput()).getDiagram();
+    final URI originalURI = diagram.eResource().getURI();
+    final URI parentURI = originalURI.trimSegments(1);
+    final String REGEX_FILENAME = "\\" + "$originalFile" + "";
+
+    String finalSegment = "$originalFile" + ".bpmn20.xml";
+    finalSegment = finalSegment.replaceAll(REGEX_FILENAME, originalURI.lastSegment());
+
+    return parentURI.appendSegment(finalSegment);
+  }
+
+  private IFile getAssociatedDiagramIFile(final IFile bpmnFile) {
+
+    IPath path = getAssociatedDiagramURI(bpmnFile);
+
+    final IWorkspace workspace = ResourcesPlugin.getWorkspace();
+    final IFile diagramFile = workspace.getRoot().getFile(path);
+
+    return diagramFile;
+  }
+
+  private IPath getAssociatedDiagramURI(final IFile bpmnFile) {
+
+    final IPath originalPath = bpmnFile.getFullPath();
+
+    final IPath parentPath = originalPath.removeLastSegments(1);
+
+    String finalSegment = originalPath.lastSegment();
+    finalSegment = finalSegment.replace(".activiti.bpmn20.xml", ".activiti");
+    final IPath returnPath = parentPath.append(new Path(finalSegment));
+    return returnPath;
+  }
+
+  private IFileEditorInput getBPMN2EditorInput() {
+    if (associatedBPMN2File != null && associatedBPMN2File.exists()) {
+      return new FileEditorInput(associatedBPMN2File);
+    } else {
+      return new FileEditorInput(getAssociatedBPMN2IFile());
+    }
+  }
+
+  private boolean isBPM2FileType(final IFileEditorInput editorInput) {
+
+    boolean isBPMN2File = false;
+    IFile file = editorInput.getFile();
+    try {
+      IContentDescription desc = file.getContentDescription();
+      if (desc != null) {
+        IContentType type = desc.getContentType();
+        if (ActivitiBPMNDiagramConstants.BPMN2_CONTENTTYPE_ID.equals(type.getId())) {
+          isBPMN2File = true;
+        }
+      }
+    } catch (CoreException e) {
+      e.printStackTrace();
+      return isBPMN2File;
+    }
+
+    return isBPMN2File;
+  }
 }
