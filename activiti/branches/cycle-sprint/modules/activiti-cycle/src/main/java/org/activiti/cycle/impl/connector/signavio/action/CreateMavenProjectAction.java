b9a8c4d384792c768d959ce347a22461b0bf25a2
meyerd <meyerd@51b7ce9a-3489-0410-86de-dc9a27cf12c7>
2011-02-23 18:12:13 +0000

    Adjusted CreateMavenProjectAction such that it can create multiple technical models

--- activiti/branches/cycle-sprint/modules/activiti-cycle/src/main/java/org/activiti/cycle/impl/connector/signavio/action/CreateMavenProjectAction.java
+++ activiti/branches/cycle-sprint/modules/activiti-cycle/src/main/java/org/activiti/cycle/impl/connector/signavio/action/CreateMavenProjectAction.java
@@ -3,21 +3,25 @@ package org.activiti.cycle.impl.connector.signavio.action;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
 
 import org.activiti.cycle.Content;
+import org.activiti.cycle.CycleComponentFactory;
 import org.activiti.cycle.RepositoryArtifact;
 import org.activiti.cycle.RepositoryArtifactLink;
 import org.activiti.cycle.RepositoryConnector;
 import org.activiti.cycle.RepositoryException;
 import org.activiti.cycle.annotations.CycleComponent;
-import org.activiti.cycle.annotations.ExcludesCycleComponents;
 import org.activiti.cycle.context.CycleContextType;
-import org.activiti.cycle.impl.action.DefaultCopyArtifactAction;
+import org.activiti.cycle.impl.components.RuntimeConnectorList;
 import org.activiti.cycle.impl.connector.signavio.provider.ActivitiCompliantBpmn20Provider;
 import org.activiti.cycle.impl.connector.util.TransactionalConnectorUtils;
 import org.activiti.cycle.impl.db.entity.RepositoryArtifactLinkEntity;
@@ -47,7 +51,7 @@ public class CreateMavenProjectAction extends CreateTechnicalBpmnXmlAction {
     super("Create default maven project");
     try {
       ACTIVITI_HOME_PATH = new File("../../../").getCanonicalPath();
-    } catch (IOException e) {      
+    } catch (IOException e) {
       ACTIVITI_HOME_PATH = new File(".").getAbsolutePath();
       log.log(Level.WARNING, "Couldn't set Activiti Home path (see nested exception for the cause), using '" + ACTIVITI_HOME_PATH + "' instead.", e);
     }
@@ -62,34 +66,39 @@ public class CreateMavenProjectAction extends CreateTechnicalBpmnXmlAction {
     RepositoryConnector targetConnector = (RepositoryConnector) getParameter(parameters, PARAM_TARGET_CONNECTOR, true, null, RepositoryConnector.class);
     boolean createLink = (Boolean) getParameter(parameters, CREATE_LINK_NAME, true, Boolean.TRUE, Boolean.class);
 
-    String bpmnXml = ActivitiCompliantBpmn20Provider.createBpmnXml(connector, artifact);
+    List<RepositoryArtifact> processes = new ArrayList<RepositoryArtifact>();
+    processes.add(artifact);
+    createMavenProject(targetFolderId, targetName, comment, targetConnector, createLink, processes);
 
-    RepositoryArtifact bpmnArtifact = null;
+  }
+  public void createMavenProject(String targetFolderId, String targetName, String comment, RepositoryConnector targetConnector, boolean createLink,
+          List<RepositoryArtifact> processes) {
+    Map<RepositoryArtifact, RepositoryArtifact> bpmnArtifacts = null;
     TransactionalConnectorUtils.beginTransaction(targetConnector);
     try {
       // create maven project
-      bpmnArtifact = createProject(targetConnector, targetFolderId, targetName, bpmnXml);
+      bpmnArtifacts = createProject(targetConnector, targetFolderId, targetName, processes);
       TransactionalConnectorUtils.commitTransaction(targetConnector, comment);
     } catch (RepositoryException e) {
       TransactionalConnectorUtils.rollbackTransaction(targetConnector);
       throw e;
     }
 
-    if (createLink && bpmnArtifact != null) {
+    if (createLink && bpmnArtifacts != null) {
       // TODO: We cannot link to a folder at the moment!
       // RepositoryFolder targetFolder =
       // targetConnector.getRepositoryFolder(targetFolderId);
-
-      RepositoryArtifactLink link = new RepositoryArtifactLinkEntity();
-      link.setSourceArtifact(artifact);
-      link.setTargetArtifact(bpmnArtifact);
-      link.setComment(comment);
-      link.setLinkType(getLinkType());
-      CycleRepositoryService repositoryService = CycleServiceFactory.getRepositoryService();
-      repositoryService.addArtifactLink(link);
+      for (Entry<RepositoryArtifact, RepositoryArtifact> processMappedToXml : bpmnArtifacts.entrySet()) {
+        RepositoryArtifactLink link = new RepositoryArtifactLinkEntity();
+        link.setSourceArtifact(processMappedToXml.getKey());
+        link.setTargetArtifact(processMappedToXml.getValue());
+        link.setComment(comment);
+        link.setLinkType(getLinkType());
+        CycleRepositoryService repositoryService = CycleServiceFactory.getRepositoryService();
+        repositoryService.addArtifactLink(link);
+      }
     }
   }
-
   public String getProcessName(RepositoryArtifact artifact) {
     return artifact.getMetadata().getName();
   }
@@ -98,8 +107,9 @@ public class CreateMavenProjectAction extends CreateTechnicalBpmnXmlAction {
    * create a project from the Maven template and return the RepositoryArtifact
    * representing the bpmn process model
    */
-  public RepositoryArtifact createProject(RepositoryConnector connector, String rootFolderId, String projectName, String processDefinitionXml) {
-    RepositoryArtifact result = null;
+  public Map<RepositoryArtifact, RepositoryArtifact> createProject(RepositoryConnector connector, String rootFolderId, String projectName,
+          List<RepositoryArtifact> processes) {
+    Map<RepositoryArtifact, RepositoryArtifact> resultList = null;
     try {
       ZipInputStream projectTemplateInputStream = new ZipInputStream(getProjectTemplate());
       ZipEntry zipEntry = null;
@@ -132,24 +142,15 @@ public class CreateMavenProjectAction extends CreateTechnicalBpmnXmlAction {
         if (zipEntry.isDirectory()) {
           connector.createFolder(absolutePath, name);
         } else {
-          Content content = new Content();
-
           if ("template.bpmn20.xml".equals(name)) {
-            // This file shall be replaced with the process
-            // definition
-            content.setValue(processDefinitionXml);
-            name = projectName + ".bpmn20.xml";
-            isBpmnModel = true;
-            log.log(Level.INFO, "Create processdefinition from Signavio process model " + projectName);
+            resultList = generateProcesses(processes, absolutePath, connector.getId());
           } else {
+            Content content = new Content();
             byte[] bytes = IoUtil.readInputStream(projectTemplateInputStream, "ZIP entry '" + zipName + "'");
             String txtContent = new String(bytes).replaceAll(REPLACE_STRING, projectName).replaceAll("@@ACTIVITI.HOME@@", ACTIVITI_HOME_PATH);
             content.setValue(txtContent);
-          }
-          log.log(Level.INFO, "Create new artifact from zip entry '" + zipEntry.getName() + "' in folder '" + absolutePath + "' with name '" + name + "'");
-          RepositoryArtifact artifact = connector.createArtifact(absolutePath, name, null, content);
-          if (isBpmnModel) {
-            result = artifact;
+            log.log(Level.INFO, "Create new artifact from zip entry '" + zipEntry.getName() + "' in folder '" + absolutePath + "' with name '" + name + "'");
+            connector.createArtifact(absolutePath, name, null, content);
           }
         }
         projectTemplateInputStream.closeEntry();
@@ -158,9 +159,27 @@ public class CreateMavenProjectAction extends CreateTechnicalBpmnXmlAction {
     } catch (IOException ex) {
       throw new RepositoryException("Couldn't create maven project due to IO errors", ex);
     }
-    return result;
+    return resultList;
   }
 
+  /**
+   * generates bpmn20.xml artifacts for the provided process models
+   */
+  protected Map<RepositoryArtifact, RepositoryArtifact> generateProcesses(List<RepositoryArtifact> processes, String path, String targetConnectorId) {
+    Map<RepositoryArtifact, RepositoryArtifact> resultList = new HashMap<RepositoryArtifact, RepositoryArtifact>();
+    RuntimeConnectorList runtimeConnectorList = CycleComponentFactory.getCycleComponentInstance(RuntimeConnectorList.class, RuntimeConnectorList.class);
+    for (RepositoryArtifact process : processes) {
+      log.log(Level.INFO, "Create processdefinition from Signavio process model " + process.getMetadata().getName());
+      String name = getProcessName(process) + ".bpmn20.xml";
+      Content content = new Content();
+      RepositoryConnector connector = runtimeConnectorList.getConnectorById(process.getConnectorId());
+      content.setValue(ActivitiCompliantBpmn20Provider.createBpmnXml(connector, process));
+      RepositoryConnector targetConnector = runtimeConnectorList.getConnectorById(targetConnectorId);
+      resultList.put(process, targetConnector.createArtifact(path, name, null, content));
+    }
+    return resultList;
+
+  }
   protected InputStream getProjectTemplate() {
     return this.getClass().getResourceAsStream("activiti-cycle-maven-template.zip");
   }
