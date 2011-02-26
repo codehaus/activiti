544c1104256e0c75ea83822fa32306e56501c14b
meyerd <meyerd@51b7ce9a-3489-0410-86de-dc9a27cf12c7>
2011-02-26 10:35:47 +0000

    Fixing ConnectorLogininterceptor, now trying once to login with (potentially) provided credentials.

--- activiti/branches/cycle-sprint/modules/activiti-cycle/src/main/java/org/activiti/cycle/impl/connector/ConnectorLoginInterceptor.java
+++ activiti/branches/cycle-sprint/modules/activiti-cycle/src/main/java/org/activiti/cycle/impl/connector/ConnectorLoginInterceptor.java
@@ -3,6 +3,8 @@ package org.activiti.cycle.impl.connector;
 import java.lang.reflect.Method;
 import java.util.SortedSet;
 import java.util.TreeSet;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import org.activiti.cycle.RepositoryAuthenticationException;
 import org.activiti.cycle.RepositoryConnector;
@@ -14,6 +16,8 @@ import org.activiti.cycle.impl.Interceptor;
  * @author daniel.meyer@camunda.com
  */
 public class ConnectorLoginInterceptor implements Interceptor {
+
+  private Logger log = Logger.getLogger(ConnectorLoginInterceptor.class.getName());
 
   // set of method-names we do not want to intercept calls to (check whether the
   // connector is loggedin)
@@ -43,13 +47,22 @@ public class ConnectorLoginInterceptor implements Interceptor {
     // for all other methods, check whether the connector is logged in.
     if (!connector.isLoggedIn()) {
       // the connector is not logged in, try to login:
+      boolean loginSuccessful = false;
+      Exception exception = null;
       try {
         PasswordEnabledRepositoryConnector pwEnabledRepositoryConnector = (PasswordEnabledRepositoryConnector) connector;
         String username = pwEnabledRepositoryConnector.getUsername();
         String password = pwEnabledRepositoryConnector.getPassword();
-        pwEnabledRepositoryConnector.login(username, password);
+        loginSuccessful = pwEnabledRepositoryConnector.login(username, password);
       } catch (Exception e) {
-        // we cannot login with the provided credentials, throw exception
+        log.log(Level.WARNING, "Could not login connector '" + connector.getName() + "' with provided credentials.", e);
+        // we cannot login with the provided credentials
+        exception = e;
+      }
+      if (!loginSuccessful) {
+        if (exception != null) {
+          throw new RepositoryAuthenticationException("Connector '" + connector.getName() + "' is not logged in ", connector.getId(), exception);
+        }
         throw new RepositoryAuthenticationException("Connector '" + connector.getName() + "' is not logged in ", connector.getId());
       }
     }
