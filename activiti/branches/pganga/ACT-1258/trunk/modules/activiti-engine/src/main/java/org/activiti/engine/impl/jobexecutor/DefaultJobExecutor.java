fdadea25f5f058e3a499d99f59c329bc1f13a552
pganga <pganga@51b7ce9a-3489-0410-86de-dc9a27cf12c7>
2012-06-26 13:29:50 +0000

    ACT-1258 extracted reusable methods that can be used in SpringJobExecutor.

--- activiti/branches/pganga/ACT-1258/trunk/modules/activiti-engine/src/main/java/org/activiti/engine/impl/jobexecutor/DefaultJobExecutor.java
+++ activiti/branches/pganga/ACT-1258/trunk/modules/activiti-engine/src/main/java/org/activiti/engine/impl/jobexecutor/DefaultJobExecutor.java
@@ -24,116 +24,120 @@ import java.util.logging.Logger;
 import org.activiti.engine.ActivitiException;
 
 /**
- * <p>This is a simple implementation of the {@link JobExecutor} using self-managed
- * threads for performing background work.</p>
+ * <p>
+ * This is a simple implementation of the {@link JobExecutor} using self-managed
+ * threads for performing background work.
+ * </p>
  * 
- * <p>This implementation uses a {@link ThreadPoolExecutor} backed by a queue to which
- * work is submitted.</p>
+ * <p>
+ * This implementation uses a {@link ThreadPoolExecutor} backed by a queue to
+ * which work is submitted.
+ * </p>
  * 
- * <p><em>NOTE: use this class in environments in which self-management of threads 
+ * <p>
+ * <em>NOTE: use this class in environments in which self-management of threads 
  * is permitted. Consider using a different thread-management strategy in 
- * J(2)EE-Environments.</em></p>
+ * J(2)EE-Environments.</em>
+ * </p>
  * 
  * @author Daniel Meyer
  */
 public class DefaultJobExecutor extends JobExecutor {
-  
-  private static Logger log = Logger.getLogger(DefaultJobExecutor.class.getName());
-  
-  protected int queueSize = 3;
-  protected int corePoolSize = 3;
-  private int maxPoolSize = 10;
 
-  protected Thread jobAcquisitionThread;
-  protected BlockingQueue<Runnable> threadPoolQueue;
-  protected ThreadPoolExecutor threadPoolExecutor;
-    
-  protected void startExecutingJobs() {
-    if (threadPoolQueue==null) {
-      threadPoolQueue = new ArrayBlockingQueue<Runnable>(queueSize);
-    }
-    if (threadPoolExecutor==null) {
-      threadPoolExecutor = new ThreadPoolExecutor(corePoolSize, maxPoolSize, 0L, TimeUnit.MILLISECONDS, threadPoolQueue);      
-      threadPoolExecutor.setRejectedExecutionHandler(new ThreadPoolExecutor.AbortPolicy());
-    }
-    if(jobAcquisitionThread == null) {
-      jobAcquisitionThread = new Thread(acquireJobsRunnable);
-      jobAcquisitionThread.start();      
-    }   
-  }
-    
-  protected void stopExecutingJobs() {
-    try {
-      jobAcquisitionThread.join();
-    }catch (InterruptedException e) {
-      log.log(Level.WARNING, "Interrupted while waiting for the job Acquisition thread to terminate", e);
-    }
-    
-    // Ask the thread pool to finish and exit
-    threadPoolExecutor.shutdown();
+	private static Logger log = Logger.getLogger(DefaultJobExecutor.class
+			.getName());
 
-    // Waits for 1 minute to finish all currently executing jobs
-    try {
-      if(!threadPoolExecutor.awaitTermination(60L, TimeUnit.SECONDS)) {
-        log.log(Level.WARNING, "Timeout during shutdown of job executor. "
-                + "The current running jobs could not end within 60 seconds after shutdown operation.");        
-      }              
-    } catch (InterruptedException e) {
-      log.log(Level.WARNING, "Interrupted while shutting down the job executor. ", e);
-    }
+	protected int queueSize = 3;
+	protected int corePoolSize = 3;
+	private int maxPoolSize = 10;
 
-    threadPoolExecutor = null;
-    jobAcquisitionThread = null;
-  }
-  
-  public void executeJobs(List<String> jobIds) {
-    try {
-      threadPoolExecutor.execute(new ExecuteJobsRunnable(this, jobIds));
-    }catch (RejectedExecutionException e) {
-      rejectedJobsHandler.jobsRejected(this, jobIds);
-    }
-  }
-  
-  // getters and setters ////////////////////////////////////////////////////// 
-  
-  public int getQueueSize() {
-    return queueSize;
-  }
-  
-  public void setQueueSize(int queueSize) {
-    this.queueSize = queueSize;
-  }
-  
-  public int getCorePoolSize() {
-    return corePoolSize;
-  }
-  
-  public void setCorePoolSize(int corePoolSize) {
-    this.corePoolSize = corePoolSize;
-  }
+	protected BlockingQueue<Runnable> threadPoolQueue;
+	protected ThreadPoolExecutor threadPoolExecutor;
 
-  public int getMaxPoolSize() {
-    return maxPoolSize;
-  }
+	protected void startExecutingJobs() {
+		if (threadPoolQueue == null) {
+			threadPoolQueue = new ArrayBlockingQueue<Runnable>(queueSize);
+		}
+		if (threadPoolExecutor == null) {
+			threadPoolExecutor = new ThreadPoolExecutor(corePoolSize,
+					maxPoolSize, 0L, TimeUnit.MILLISECONDS, threadPoolQueue);
+			threadPoolExecutor
+					.setRejectedExecutionHandler(new ThreadPoolExecutor.AbortPolicy());
+		}
+		startJobAcquisitionThread();
+	}
 
-  public void setMaxPoolSize(int maxPoolSize) {
-    this.maxPoolSize = maxPoolSize;
-  }
-  
-  public BlockingQueue<Runnable> getThreadPoolQueue() {
-    return threadPoolQueue;
-  }
+	protected void stopExecutingJobs() {
 
-  public void setThreadPoolQueue(BlockingQueue<Runnable> threadPoolQueue) {
-    this.threadPoolQueue = threadPoolQueue;
-  }
+		stopJobAcquisitionThread();
 
-  public ThreadPoolExecutor getThreadPoolExecutor() {
-    return threadPoolExecutor;
-  }
-  
-  public void setThreadPoolExecutor(ThreadPoolExecutor threadPoolExecutor) {
-    this.threadPoolExecutor = threadPoolExecutor;
-  }
-    
+		// Ask the thread pool to finish and exit
+		threadPoolExecutor.shutdown();
+
+		// Waits for 1 minute to finish all currently executing jobs
+		try {
+			if (!threadPoolExecutor.awaitTermination(60L, TimeUnit.SECONDS)) {
+				log.log(Level.WARNING,
+						"Timeout during shutdown of job executor. "
+								+ "The current running jobs could not end within 60 seconds after shutdown operation.");
+			}
+		} catch (InterruptedException e) {
+			log.log(Level.WARNING,
+					"Interrupted while shutting down the job executor. ", e);
+		}
+
+		threadPoolExecutor = null;
+	}
+
+	public void executeJobs(List<String> jobIds) {
+		try {
+			threadPoolExecutor.execute(new ExecuteJobsRunnable(this, jobIds));
+		} catch (RejectedExecutionException e) {
+			rejectedJobsHandler.jobsRejected(this, jobIds);
+		}
+	}
+
+	// getters and setters
+	// //////////////////////////////////////////////////////
+
+	public int getQueueSize() {
+		return queueSize;
+	}
+
+	public void setQueueSize(int queueSize) {
+		this.queueSize = queueSize;
+	}
+
+	public int getCorePoolSize() {
+		return corePoolSize;
+	}
+
+	public void setCorePoolSize(int corePoolSize) {
+		this.corePoolSize = corePoolSize;
+	}
+
+	public int getMaxPoolSize() {
+		return maxPoolSize;
+	}
+
+	public void setMaxPoolSize(int maxPoolSize) {
+		this.maxPoolSize = maxPoolSize;
+	}
+
+	public BlockingQueue<Runnable> getThreadPoolQueue() {
+		return threadPoolQueue;
+	}
+
+	public void setThreadPoolQueue(BlockingQueue<Runnable> threadPoolQueue) {
+		this.threadPoolQueue = threadPoolQueue;
+	}
+
+	public ThreadPoolExecutor getThreadPoolExecutor() {
+		return threadPoolExecutor;
+	}
+
+	public void setThreadPoolExecutor(ThreadPoolExecutor threadPoolExecutor) {
+		this.threadPoolExecutor = threadPoolExecutor;
+	}
+
 }
